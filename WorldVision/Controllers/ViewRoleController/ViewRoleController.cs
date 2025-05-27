0016ad17c0019c5aef001a268e1bcc46ad291478
Micleusanu Dumitru <dumitru.micleusanu@isa.utm.md>
2025-05-27 15:01:42 +0300

    add controller for view addcategory and addnews

---
+++ WorldVision/Controllers/ViewRoleController/ViewRoleController.cs
@@ -0,0 +1,16 @@
+using System.Web.Mvc;
+
+namespace WorldVision.Web.Controllers
+{
+    public class ViewRoleController : Controller
+    {
+        public ActionResult AddNews()
+        {
+            return View();
+        }
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+    }
+}
